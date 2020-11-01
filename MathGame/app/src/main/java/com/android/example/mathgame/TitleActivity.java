4135556c9408996d7fa8bda9f9ad43b5ab30ac31
merick1893 <69695621+merick1893@users.noreply.github.com>
2020-11-01 13:00:34 -0600

    Updated Title Screen and Misc
    Updated the title screen to link to the high score screen and various other files to resolve issues.

--- MathGame/app/src/main/java/com/android/example/mathgame/TitleActivity.java
+++ MathGame/app/src/main/java/com/android/example/mathgame/TitleActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class TitleActivity extends AppCompatActivity {
     private Button levelSelect;
+    private Button highScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +23,23 @@ public class TitleActivity extends AppCompatActivity {
                 openLevelSelectActivity();
             }
         });
+
+        highScore = findViewById(R.id.highScore);
+        highScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHighScoreActivity();
+            }
+        });
     }
 
     private void openLevelSelectActivity() {
         Intent intent = new Intent(this, LevelSelectActivity.class);
+        startActivity(intent);
+    }
+
+    private void openHighScoreActivity() {
+        Intent intent = new Intent(this, HighScoreActivity.class);
         startActivity(intent);
     }
 }
