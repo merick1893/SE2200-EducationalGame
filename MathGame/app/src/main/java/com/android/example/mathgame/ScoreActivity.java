13ae3b6669f9e6e061b96bb893ed48c6e1d35c91
merick1893 <69695621+merick1893@users.noreply.github.com>
2020-11-15 17:27:28 -0600

    Updated project folder
    Updated project folder as discussed.

--- MathGame/app/src/main/java/com/android/example/mathgame/ScoreActivity.java
+++ MathGame/app/src/main/java/com/android/example/mathgame/ScoreActivity.java
@@ -1,20 +1,31 @@
 package com.android.example.mathgame;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class ScoreActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        button = (Button) findViewById(R.id.button6);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPauseActivity();
+            }
+        });
+    }
+
+    public void openPauseActivity() {
+        Intent intent = new Intent(this, PauseActivity.class);
+        startActivity(intent);
     }
 }
