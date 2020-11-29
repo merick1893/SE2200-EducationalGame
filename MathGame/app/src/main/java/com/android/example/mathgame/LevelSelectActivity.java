e4308e03502cc6880404831444c692ec2ce4f9b6
merick1893 <69695621+merick1893@users.noreply.github.com>
2020-11-29 14:15:26 -0600

    Updated screens and buttons

--- MathGame/app/src/main/java/com/android/example/mathgame/LevelSelectActivity.java
+++ MathGame/app/src/main/java/com/android/example/mathgame/LevelSelectActivity.java
@@ -2,13 +2,30 @@ package com.android.example.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LevelSelectActivity extends AppCompatActivity {
+    private Button level1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
+
+        level1 = findViewById(R.id.level1);
+        level1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openScoreActivity();
+            }
+        });
+    }
+
+    private void openScoreActivity() {
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
     }
 }
--- MathGame/app/src/main/java/com/android/example/mathgame/PauseActivity.java
+++ MathGame/app/src/main/java/com/android/example/mathgame/PauseActivity.java
@@ -2,13 +2,30 @@ package com.android.example.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PauseActivity extends AppCompatActivity {
+    private Button returnToScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pause);
+
+        returnToScore = (Button) findViewById(R.id.button7);
+        returnToScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openScoreActivity();
+            }
+        });
     }
-}
+
+    public void openScoreActivity() {
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
+    }
+} // button7
