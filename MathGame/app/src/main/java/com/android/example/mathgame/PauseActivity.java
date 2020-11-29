c34c1da0de7e441d4c945d64658cfad00ff723ae
merick1893 <69695621+merick1893@users.noreply.github.com>
2020-11-29 14:53:41 -0600

    Update round 2
    Chased down a few more odds and ends in final sweeps before presentation and submission.

--- MathGame/app/src/main/java/com/android/example/mathgame/PauseActivity.java
+++ MathGame/app/src/main/java/com/android/example/mathgame/PauseActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 
 public class PauseActivity extends AppCompatActivity {
     private Button returnToScore;
+    private Button returnToLevelSelect;
+    private Button returnToTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,36 @@ public class PauseActivity extends AppCompatActivity {
                 openScoreActivity();
             }
         });
+
+        returnToLevelSelect = (Button) findViewById(R.id.goToLevelSelect);
+        returnToLevelSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLevelSelectActivity();
+            }
+        });
+
+        returnToTitle = (Button) findViewById(R.id.goToTitle);
+        returnToTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTitleActivity();
+            }
+        });
     }
 
     public void openScoreActivity() {
         Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLevelSelectActivity() {
+        Intent intent = new Intent(this, LevelSelectActivity.class);
+        startActivity(intent);
+    }
+
+    public void openTitleActivity() {
+        Intent intent = new Intent(this, TitleActivity.class);
         startActivity(intent);
     }
 } // button7
