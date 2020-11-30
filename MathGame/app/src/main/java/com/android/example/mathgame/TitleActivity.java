9abcfab95310e10aa06249afa9faa2ff43a33661
merick1893 <69695621+merick1893@users.noreply.github.com>
2020-11-29 18:02:24 -0600

    Update TitleActivity.java

--- MathGame/app/src/main/java/com/android/example/mathgame/TitleActivity.java
+++ MathGame/app/src/main/java/com/android/example/mathgame/TitleActivity.java
@@ -1,3 +1,7 @@
+/* This activity is the main activity in the program and is the jumping off point into other aspects of the app.
+*   The code below creates the links between the title screen, level select screen, and high score screen.
+ */
+
 package com.android.example.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +20,9 @@ public class TitleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
+        // Line 24 connects the button variable declared above with the levelSelect button on screen
         levelSelect = findViewById(R.id.levelSelect);
+        // Line 26 makes a call to the openLevelSelectActivity() method when the user presses the levelSelect button
         levelSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +30,7 @@ public class TitleActivity extends AppCompatActivity {
             }
         });
 
+        // Lines 34 and 35 accomplish the same as 24 and 26 above.
         highScore = findViewById(R.id.highScore);
         highScore.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +40,7 @@ public class TitleActivity extends AppCompatActivity {
         });
     }
 
+    // The following two methods open the screens referenced in their names
     private void openLevelSelectActivity() {
         Intent intent = new Intent(this, LevelSelectActivity.class);
         startActivity(intent);
